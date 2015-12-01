fc63b6011989faf0289bff3ed80af3865c7b2120
Kouki Kitajima <saburo@geology.wisc.edu>
2015-12-01 15:58:11 -0600

    Fix the errer on release with asar and prune

--- release.js
+++ release.js
@@ -1,24 +1,34 @@
 var packager = require('electron-packager');
 var config = require('./package.json');
- 
+
+var platform = 'darwin,win32';
+if (process.argv.length > 2) {
+  var tmp = process.argv[2].toLowerCase();
+  if (tmp === 'mac' || tmp === 'osx' || tmp === 'darwin') {
+    platform = 'darwin';
+  } else {
+    platform = 'win32';
+  }
+}
+
 packager({
     dir: './',
     out: '../dist',
     name: config.name,
     // platform: 'darwin,win32',
-    platform: 'darwin,win32',
+    platform: platform,
     arch: 'x64',
     version: '0.34.1',
     // version: '0.30.0',
     icon: './resources/icon', //<- アプリアイコン
- 
+
     'app-bundle-id': 'edu.wisc.geology.wiscsims', //<- 自分のドメインなどを使用してください
- 
+
     'app-version': config.version,
     'helper-bundle-id': 'edu.wisc.geology.wiscsims', //<- 自分のドメインなどを使用してください
     overwrite: true,
-    asar: false,
-    prune: false,
+    asar: true,
+    prune: true,
     ignore: "node_modules/(electron-packager|electron-prebuilt|\.bin)|release\.js|\.DS_Store",
     'version-string': {
         CompanyName: 'WiscSIMS',

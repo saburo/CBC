0abbfcd238b4008e3f16b71bb7a7b555d2c3961c
Kouki Kitajima <saburo@geology.wisc.edu>
2015-12-13 16:40:18 -0600

    Add tests for asc_parser

---
+++ src/renderer/common/asc_parser3.js
@@ -0,0 +1,317 @@
+module.exports = function() {
+  var lines = [],
+      keys = [];
+
+  function my() {}
+
+  my.init = function(asc) {
+    if (my.parseLines(asc) && my.parseKeys(lines)) return true;
+    return false;
+  };
+
+  my.parseLines = function(asc) {
+    lines = asc.replace(/\r/g, '').split(/\n/).map(function(a) {
+      return a.replace(/(\/{2,}|\={2,})/, '')
+              .replace(/\t$/, '')
+              .replace(/Option\: /, 'Option# ')
+              .replace(/\s?\:\s+/, '\t')
+              .split(/\t/).map(function(b) {
+                return sanitize(b.trim().replace(/\s?\:$/,''));
+              });
+    });
+    return true;
+  };
+  my.getLines = function() { return lines; };
+
+  my.parseKeys = function(lines) {
+    keys = lines.map(function(l) { return l[0]; });
+    return true;
+  };
+  my.getKeys = function() { return keys; };
+
+  my.getListedContents = function(keyword, opt) {
+    opt = {
+      offset: opt.offset === undefined ?  2 : opt.offset,
+      length: opt.length    === undefined ? 30 : opt.length
+    };
+    var myIndex = keys.indexOf(keyword) + opt.offset;
+    var out = {}, r = '';
+    for (var i = myIndex; i < myIndex + opt.length; i++) {
+      r = lines[i][0];
+      if (r === '') break;
+      out[r] = lines[i][1];
+    }
+
+    return out;
+  };
+
+  my.getTabulatedContents = function(keyword, opt) {
+    opt = {
+      offset: opt.offset === undefined ?  2 : opt.offset,
+      length: opt.length === undefined ? 30 : opt.length,
+       label: opt.label  === undefined ?  2 : opt.label,
+    };
+    var myIndex = keys.indexOf(keyword) + opt.offset;
+    var out = {}, rowname = '';
+    var labels = lines[myIndex - opt.label].slice(1);
+    var addOut = function(v, j) { out[this.r][v] = lines[this.i][j + 1]; };
+
+    for (var i = myIndex; i < myIndex + opt.length; i++) {
+      r = lines[i][0];
+      if (r === '') break;
+      out[r] = {};
+      labels.forEach(addOut, {r: r, i: i});
+    }
+    return out;
+  };
+
+  my.getDateTime = function() {
+    var dt = new Date(lines[0][1] + ' ' + lines[1][1]);
+    var t = parseDateTime(dt);
+    return {
+      raw: dt,
+      timestamp: dt.getTime(),
+      formatted: t.year + '/' + t.month + '/' + t.day + ' ' +
+                 t.hour + ':' + t.minute + ':00'
+    };
+  };
+
+  my.getFileInfo = function() {
+    var myIndex = keys.indexOf('ACQUISITION FILE NAME');
+    var out = {
+      ais: {path: lines[myIndex][1], name: ''},
+      asc: {path: '', name: ''},
+      dis: {path: lines[myIndex+1][1], name: ''}
+    };
+    out.ais.name = out.ais.path.split('\\').pop();
+    out.asc.path = out.ais.path.replace(/\.ais$/, '.asc');
+    out.asc.name = out.asc.path.split('\\').pop();
+    out.dis.name = out.dis.path.split('\\').pop();
+
+    return out;
+  };
+
+  my.getDescription = function() {
+    var myIndex = keys.indexOf('File Description') + 1;
+    return lines[myIndex][0].replace(/(^\"|\"$)/g, '');
+  };
+
+  my.getSampleName = function() {
+    var myIndex = keys.indexOf('SAMPLE NAME');
+    return lines[myIndex][1];
+  };
+
+  my.getStagePosition = function() {
+    var myIndex = keys.indexOf('X POSITION');
+    return {x: lines[myIndex][1], y: lines[myIndex][3]};
+  };
+  my.getAcquisitionParams = function() {
+    var opt = {offset: 4, length: 20, label: 2};
+    return my.getTabulatedContents('ACQUISITION PARAMETERS', opt);
+  };
+
+  my.getAnalyticalParams = function() {
+    var opt = {offset: 2, length: 30};
+    return my.getListedContents('ANALYTICAL PARAMETERS', opt);
+  };
+
+  my.getComments = function() {
+    return lines[keys.indexOf('COMMENTS') + 1][0];
+  };
+
+  my.getDetectorParams = function () {
+    var opt = {offset: 4, length: 20, label: 2};
+    return my.getTabulatedContents('DETECTOR PARAMETERS', opt);
+  };
+
+  my.getCorrectionFactorStatus = function () {
+    var opt = {offset: 2, length: 10};
+    return my.getListedContents('CORRECTION FACTORS FOR RATIOS COMPUTATION', opt);
+  };
+
+  my.getAcquisitionControlParams = function() {
+    var opt = {offset: 3, length: 20};
+    return my.getListedContents('ACQUISITION CONTROL PARAMETERS', opt);
+  };
+
+  my.getPresputteringParams = function() {
+    var opt = {offset: 2, length: 10};
+    return my.getListedContents('Pre-sputtering PARAMETERS', opt);
+  };
+
+  my.getIsotopicRatioDefinitions = function () {
+    var myIndex = keys.indexOf('ISOTOPICS RATIO') + 2;
+    var out = {};
+    var tmp;
+    for (var i = myIndex; i<myIndex+20; i+=2) {
+      if (lines[i][0] === '') break;
+      tmp = lines[i][0].split(/\=/);
+      out[tmp[0]] = tmp[1];
+    }
+
+    return out;
+  };
+
+  my.getCummulatedResults = function() {
+    var opt = {offset: 4, length: 10, label: 2};
+    return my.getTabulatedContents('CUMULATED RESULTS', opt);
+  };
+
+  my.getPrimaryBeamIntensity = function() {
+    var myIndex = keys.indexOf('Primary Current START (A)');
+    var out = {
+      start: lines[myIndex][1],
+      end: lines[myIndex + 1][1],
+      mean: 0, diff: 0
+    };
+    out.mean = (out.start + out.end) / 2;
+    out.diff = round(100 * (out.start - out.end) / out.mean, 3);
+
+    return out;
+  };
+
+  my.getBeamCenteringResults = function() {
+    var opt = {offset: 4, length: 10, label: 2};
+    return my.getTabulatedContents('BEAM CENTERING RESULTS', opt);
+  };
+
+  my.getCPS = function() {
+    var cpsIndex = keys.indexOf('#block') + 4,
+        cpsIndex2 = keys.indexOf('#block', cpsIndex) - 4,
+        lab = lines[cpsIndex - 2].slice(1),
+        cps = {};
+    for (var m = 0; m < lab.length; m++) {
+      cps[lab[m]] = [];
+      for (var i = cpsIndex; i <= cpsIndex2; i++) {
+        cps[lab[m]].push(lines[i][m + 2]);
+      }
+    }
+    return cps;
+  };
+
+
+  my.statCPS = function(data) {
+    var out = {};
+    var masses = Object.keys(data);
+    for (var m in masses) {
+      var mass = masses[m];
+      var cps = data[mass];
+      var n = cps.length;
+      var total = sum(cps);
+      var mean = total / n;
+      out[mass] = {
+        total: total,
+        mean: mean,
+        max: arrayMax(cps),
+        min: arrayMin(cps),
+        stdev: stdev(cps),
+        order: arrayOrder(cps)
+      };
+    }
+    return out;
+  };
+
+  my.cycleNum = function(n) {
+    var out = [];
+    for (var i = 1; i <= n; i++) {
+      out.push(i);
+    }
+    return out;
+  };
+
+  my.parseAsc = function(asc) {
+    if(!my.init(asc)) return;
+
+    var comment = my.getDescription();
+    var fName = my.getFileInfo().asc.name;
+    var cps = my.getCPS();
+    var cycleNumber = cps[Object.keys(cps)[0]].length;
+    var st = this.statCPS(cps);
+    return {
+      cps: cps,
+      cycleNumber: cycleNumber,
+      comment: comment,
+      stat: st,
+      ascName: fName,
+    };
+  };
+
+  // PRIVATE FUNCTION
+  var sum = function(data) {
+    return data.reduce(function(a, b) { return a + b; });
+  };
+
+  var mean = function(arr) {
+    return sum(arr) / arr.length;
+  };
+
+  var stdev = function(data) {
+    var n = data.length;
+    var average = mean(data);
+    var s = sum(data.map(function(a) { return Math.pow(a - average, 2); })) / (n - 1);
+    return Math.sqrt(s);
+  };
+
+  var arrayMax = function(arr) {
+    return arr.reduce(function(a, b) { return (a > b ? a : b); });
+  };
+
+  var arrayMin = function(arr) {
+    return arr.reduce(function(a, b) { return (a < b ? a : b); });
+  };
+
+  var arrayOrder = function(arr) {
+    return Math.floor(Math.log10(mean(arr)));
+  };
+
+  var sanitize = function(txt) {
+    if (!txt) return txt;
+    return txt.match(/[a-zA-DF-Z\=\s\/\'\"%$#@]/) ? txt : Number(txt);
+  };
+
+  var pad0 = function(num, n, by) {
+    n = n || 2; by = by ? Str(by) : '0';
+    num = String(num);
+    var out = '';
+    var l = num.length;
+    if (l > n) return num;
+    for (var i=0; i<n-l; i++) {
+      out += by;
+    }
+    return out + num;
+  };
+
+  var parseDateTime = function(dt) {
+    return {
+      year: pad0(dt.getFullYear()),
+      month: pad0(dt.getMonth() + 1, 2, 0),
+      day: pad0(dt.getDate(), 2 , 0),
+      hour: pad0(dt.getHours(), 2, 0),
+      minute: pad0(dt.getMinutes(), 2, 0),
+      second: pad0(dt.getSeconds(), 2, 0)
+    };
+  };
+
+  var round = function(num, digit) {
+    digit = digit || 2;
+    var scale = Math.pow(10, digit);
+    return Math.round(num * scale) / scale;
+  };
+
+  if (process.env.NODE_ENV === 'test') {
+    my._private = {
+      sum: sum,
+      mean: mean,
+      stdev: stdev,
+      arrayMax: arrayMax,
+      arrayMin: arrayMin,
+      arrayOrder: arrayOrder,
+      sanitize: sanitize,
+      pad0: pad0,
+      parseDateTime: parseDateTime,
+      round: round,
+    };
+  }
+
+  return my;
+}();
---
+++ test/test.js
@@ -0,0 +1,259 @@
+process.env.NODE_ENV = 'test';
+var ps = require('../src/renderer/common/asc_parser3');
+var fs = require('fs');
+var assert = require('assert');
+var data = fs.readFileSync(__dirname + '/data/20140624@6.asc', 'utf8');
+var line, key;
+
+describe('ASC parser', function(){
+  describe('public functions', function(){
+    describe('init parser', function() {
+      it('wait parser initialization', function() {
+        var status = ps.init(data);
+        assert.strictEqual(true, status);
+        assert.equal(238, ps.getLines().length);
+        assert.equal(238, ps.getKeys().length);
+      });
+    });
+    describe('parsing parts', function() {
+      it('parse lines', function() {
+        ps.parseLines(data);
+        line = ps.getLines();
+        assert.equal(238, line.length);
+        assert.equal('CAMECA', line[0][0]);
+        assert.equal('MASSES', line[17][0]);
+        assert.equal('Yield', line[77][0]);
+        assert.equal('APPLYED', line[77][1]);
+        assert.equal('TIMING  DATA', line[209][0]);
+      });
+
+      it('parse keys', function() {
+        ps.parseKeys(line);
+        key = ps.getKeys();
+        assert.equal(238, key.length);
+        assert.equal(true, line.length === key.length);
+        assert.equal('CAMECA', key[0]);
+        assert.equal('MASSES', key[17]);
+        assert.equal('Yield', key[77]);
+        assert.equal('TIMING  DATA', key[209]);
+      });
+
+      it('parse Date and Time', function() {
+        var t = ps.getDateTime();
+        // assert.strictEqual(1450112824000, t.timestamp);
+        assert.strictEqual('2014/06/24 16:00:00', t.formatted);
+      });
+
+      it('parse file paths and names', function() {
+        var finfo = ps.getFileInfo();
+        assert.strictEqual('D:\\Cameca IMS Datas\\cips_data\\data\\20140624_d18O_garnet_stds_Kouki\\20140624@6.ais', finfo.ais.path);
+        assert.strictEqual('20140624@6.ais', finfo.ais.name);
+        assert.strictEqual('D:\\Cameca IMS Datas\\cips_data\\data\\20140624_d18O_garnet_stds_Kouki\\20140624@6.asc', finfo.asc.path);
+        assert.strictEqual('20140624@6.asc', finfo.asc.name);
+        assert.strictEqual('D:\\Cameca IMS Datas\\cips_data\\data\\20140624_d18O_garnet_stds_Kouki\\d18O_OH_CFC2H1.dis', finfo.dis.path);
+        assert.strictEqual('d18O_OH_CFC2H1.dis', finfo.dis.name);
+      });
+
+      it('parse file paths and names', function() {
+        assert.strictEqual('WI-STD-70 UWG-2 g1', ps.getDescription());
+      });
+
+      it('parse sample name', function() {
+        assert.strictEqual('84.5_a2_3/', ps.getSampleName());
+      });
+
+      it('parse stage positions', function() {
+        var pos = ps.getStagePosition();
+        assert.strictEqual(-1703, pos.x);
+        assert.strictEqual(295, pos.y);
+      });
+
+      it('parse acquisition params', function() {
+        var params = ps.getAcquisitionParams();
+        var k = Object.keys(params);
+        assert.strictEqual(11, k.length);
+        assert.strictEqual('DETECTOR', k[8]);
+        assert.deepEqual(['16O', '16O 1H', '18O'], Object.keys(params.MASSES));
+        assert.strictEqual(15.994915, params.MASSES['16O']);
+        assert.strictEqual('Fc', params.TYPE['18O']);
+      });
+
+      it('parse analytical params', function() {
+        var params = ps.getAnalyticalParams();
+        var k = Object.keys(params);
+        assert.strictEqual(21, k.length);
+        assert.strictEqual('SAMPLE HV   (v)', k[0]);
+        assert.strictEqual(-10000, params['SAMPLE HV   (v)']);
+        assert.strictEqual(4998, params['MRP(mono)']);
+        assert.strictEqual(750, params['L4 Aperture   (um)']);
+      });
+
+      it('parse comments', function() {
+        assert.strictEqual('""', ps.getComments());
+      });
+
+      it('parse detector params', function() {
+        var params = ps.getDetectorParams();
+        var k = Object.keys(params);
+        assert.strictEqual(10, k.length);
+        assert.strictEqual('L\'2', k[0]);
+        assert.strictEqual('FC2', k[9]);
+        assert.deepEqual('DT(ns)', Object.keys(params.L2)[2]);
+        assert.strictEqual(-2589204, params.C['Bkg(c/s)']);
+        assert.strictEqual(-2048, params.EM['Rep b']);
+      });
+
+      it('parse correction factor status', function() {
+        var params = ps.getCorrectionFactorStatus();
+        var k = Object.keys(params);
+        assert.strictEqual(6, k.length);
+        assert.strictEqual('Yield', k[0]);
+        assert.strictEqual('APPLYED', params.Background);
+        assert.strictEqual('NOT APPLYED', params['Ip Normalize']);
+      });
+
+      it('parse acuisition control params', function() {
+        var params = ps.getAcquisitionControlParams();
+        var k = Object.keys(params);
+        assert.strictEqual(8, k.length);
+        assert.strictEqual('Pre-sputtering', k[0]);
+        assert.strictEqual('Ip control', k[7]);
+        assert.strictEqual('SELECTED', params['Pre-sputtering']);
+        assert.strictEqual('NOT SELECTED', params['Ip control']);
+      });
+
+      it('parse pre-sputtering params', function() {
+        var params = ps.getPresputteringParams();
+        var k = Object.keys(params);
+        assert.strictEqual(5, k.length);
+        assert.strictEqual('Sputter Time (s)', k[0]);
+        assert.strictEqual('Ip preset end', k[4]);
+        assert.strictEqual(10, params['Sputter Time (s)']);
+        assert.strictEqual('Current', params['Ip preset end']);
+      });
+
+      it('parse isotopic ratio definitions', function() {
+        var params = ps.getIsotopicRatioDefinitions();
+        var k = Object.keys(params);
+        assert.strictEqual(3, k.length);
+        assert.strictEqual('R0', k[0]);
+        assert.strictEqual('1.000000 *16O [C] / 1.000000 *', params.R0);
+        assert.strictEqual('R2', k[2]);
+        assert.strictEqual('1.000000 *16O 1H [FC2] / 1.000000 *16O [C]', params.R2);
+      });
+
+      it('parse cummulated results', function() {
+        var params = ps.getCummulatedResults();
+        var k = Object.keys(params);
+        assert.strictEqual(3, k.length);
+        assert.strictEqual('R0', k[0]);
+        assert.strictEqual('R2', k[2]);
+        assert.deepEqual('Delta Value(permil)', Object.keys(params.R0)[6]);
+        assert.strictEqual(2.769268E+9, params.R0['Mean value']);
+        assert.strictEqual(7.756086E+0, params.R1['Delta Value(permil)']);
+      });
+
+      it('parse primary beam intensity', function() {
+        var pb = ps.getPrimaryBeamIntensity();
+        assert.strictEqual(2.135734E-9, pb.start);
+        assert.strictEqual(2.107493E-9, pb.end);
+        assert.strictEqual(2.1216135E-9, pb.mean);
+        assert.strictEqual(1.331, ps._private.round(pb.diff, 3));
+      });
+
+      it('parse beam centering results', function() {
+        var res = ps.getBeamCenteringResults();
+        var k = Object.keys(res);
+        assert.strictEqual(4, k.length);
+        assert.strictEqual('Field App (DT1)', k[0]);
+        assert.strictEqual('Option# DT FA_X', k[3]);
+        assert.strictEqual('Selected', Object.keys(res['Field App (DT1)'])[0]);
+        assert.strictEqual('resultY', Object.keys(res['Field App (DT1)'])[3]);
+        assert.strictEqual(-7, res['Field App (DT1)'].resultX);
+        assert.strictEqual(-2, res['Field App (DT1)'].resultY);
+        assert.strictEqual('no', res['Option# DT FA_X'].Selected);
+      });
+
+      it('parse cps data', function() {
+        var cps = ps.getCPS();
+        var k = Object.keys(cps);
+        assert.strictEqual(3, k.length);
+        assert.strictEqual(20, Object.keys(cps[k[0]]).length);
+        assert.deepEqual(['16O', '16O 1H', '18O'], Object.keys(cps));
+        assert.strictEqual(5.786162E+6, cps['18O'][17]);
+      });
+    });
+
+  });
+
+
+  describe('private functions', function(){
+
+    it('sum of numbers in array', function() {
+      // assert.deepEqual(, ps._private.sum([1,2,3]));
+      assert.strictEqual(6, ps._private.sum([1,2,3]));
+      assert.strictEqual(0, ps._private.sum([1,2,-3]));
+    });
+
+    it('find max value of numbers in array', function() {
+      assert.strictEqual(3, ps._private.arrayMax([1,2,3]));
+      assert.strictEqual(2, ps._private.arrayMax([1,2,-3]));
+    });
+
+    it('find min value of numbers in array', function() {
+      assert.strictEqual(1, ps._private.arrayMin([1,2,3]));
+      assert.strictEqual(-3, ps._private.arrayMin([1,2,-3]));
+    });
+
+    it('calc mean value of numbers in array', function() {
+      assert.strictEqual(2, ps._private.mean([1,2,3]));
+      assert.strictEqual(0, ps._private.mean([1,2,-3]));
+    });
+
+    it('calc standard deviation of numbers in array', function() {
+      assert.strictEqual(1, ps._private.stdev([1,2,3]));
+      assert.strictEqual(2.6457513110645907, ps._private.stdev([1,2,-3]));
+    });
+
+    it('calc mean digit number of large numbers in array', function() {
+      assert.strictEqual(3, ps._private.arrayOrder([1000, 2000, 3000]));
+      assert.strictEqual(5, ps._private.arrayOrder([100000, 200000, 300000]));
+      assert.strictEqual(-2, ps._private.arrayOrder([0.01, 0.02, 0.03]));
+    });
+
+    it('convert String to Number type when it is Number type', function() {
+      assert.strictEqual('number', typeof ps._private.sanitize('2.769268E+9'));
+      assert.strictEqual(2.769268E+9, ps._private.sanitize('2.769268E+9'));
+      assert.strictEqual('string', typeof ps._private.sanitize('CUMULATED RESULTS'));
+      assert.strictEqual('CUMULATED RESULTS', ps._private.sanitize('CUMULATED RESULTS'));
+      assert.strictEqual('', ps._private.sanitize(''));
+      assert.strictEqual('""', ps._private.sanitize('""'));
+      assert.strictEqual(null, ps._private.sanitize(null));
+      assert.strictEqual(undefined, ps._private.sanitize(undefined));
+    });
+
+    it('padded by 0', function() {
+      assert.strictEqual('02', ps._private.pad0(2,2));
+      assert.strictEqual('10', ps._private.pad0(10,2));
+      assert.strictEqual('002', ps._private.pad0(2,3));
+    });
+
+    it('parse date and time', function() {
+      var dt = new Date('Sat Dec 14 2015 11:07:04 GMT-0600');
+      var d = ps._private.parseDateTime(dt);
+      assert.strictEqual('2015', d.year);
+      assert.strictEqual('12', d.month);
+      assert.strictEqual('14', d.day);
+      assert.strictEqual('11', d.hour);
+      assert.strictEqual('07', d.minute);
+      assert.strictEqual('04', d.second);
+    });
+
+    it('calc rounding with specified digits', function() {
+      assert.strictEqual(2.1, ps._private.round(2.11, 1));
+      assert.strictEqual(2.125, ps._private.round(2.1245, 3));
+      assert.strictEqual(2, ps._private.round(2, 3));
+    });
+  });
+
+});
